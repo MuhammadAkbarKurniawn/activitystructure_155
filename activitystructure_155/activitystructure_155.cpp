19c59ba929986aa45acb97b427df42a73ac8ed08
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2023-04-03 14:36:41 +0700

    Membuat fungsi input Mahasiswa

--- activitystructure_155/activitystructure_155.cpp
+++ activitystructure_155/activitystructure_155.cpp
@@ -11,6 +11,21 @@ struct Mahasiswa {
 
 int main()
 {
-  
+    Mahasiswa mhs1, mhs2;
+
+    mhs1.nim = "20220140155";
+    mhs1.nama = "Akbarrr";
+    mhs1.alamat = "Yogyakarta";
+    mhs1.umur = 19; 
+
+    cout << "Masukkan nim: ";
+    cin >> mhs2.nim;
+    cout << "Masukkan nama: ";
+    cin >> mhs2.nama;
+    cout << "Masukkan alamat: ";
+    cin >> mhs2.alamat;
+    cout << "Masukkan umur: ";
+    cin >> mhs2.umur;
+
 }
 
