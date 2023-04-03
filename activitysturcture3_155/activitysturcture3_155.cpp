914de9edfd8e262ae3c822c7be8472242dda7ed2
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2023-04-03 15:54:24 +0700

    Memperbaiki Tampilan

--- activitysturcture3_155/activitysturcture3_155.cpp
+++ activitysturcture3_155/activitysturcture3_155.cpp
@@ -38,7 +38,7 @@ int main()
 
     for (int i = 0; i < 3; i++)
     {
-        cout << "\nNim : " << mhs[i].nim;
+        cout << "\n\nNim : " << mhs[i].nim;
         cout << "\nNama : " << mhs[i].nama;
         cout << "\ndesa : " << mhs[i].alamat.desa;
         cout << "\nKota : " << mhs[i].alamat.kota;
