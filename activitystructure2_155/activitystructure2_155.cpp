90de10f3d2385960c9404a712fe8f8b3e8979e94
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2023-04-03 15:09:34 +0700

    Membuat fungsi tampilan input dan output

--- activitystructure2_155/activitystructure2_155.cpp
+++ activitystructure2_155/activitystructure2_155.cpp
@@ -18,5 +18,23 @@ struct Mahasiswa {
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Mahasiswa mhs;
+
+    cout << "Masukkan nim: ";
+    cin >> mhs.nim;
+    cout << "Masukkan nama: ";
+    cin >> mhs.nama;
+    cout << "Alamat: " << endl;
+    cout << "\tNama Desa: ";
+    cin >> mhs.alamat.desa;
+    cout << "\tNama Kota: ";
+    cin >> mhs.alamat.kota;
+    cout << "Masukkan Umur: ";
+    cin >> mhs.umur;
+
+    cout << "\nNim : " << mhs.nim;
+    cout << "\nNama : " << mhs.nama;
+    cout << "\nNdesa : " << mhs.alamat.desa;
+    cout << "\nKota : " << mhs.alamat.kota;
+    cout << "\nUmur : " << mhs.umur;
 }
